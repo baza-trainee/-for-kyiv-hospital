d5a28eef0ec0f8f06c00d051eb1fa5b4493254ed
Oderen~ <oleksandr.deren02@gmail.com>
2023-11-12 21:50:55 +0200

    feat: adapted to 1280px

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -7,7 +7,7 @@ import { CgMenuRound } from 'react-icons/cg';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 const Header = () => {
-  const [isMenu, setIsMenu] = useState(false);
+  const [isMenu, setMenu] = useState(false);
 
   return (
     <>
@@ -16,55 +16,56 @@ const Header = () => {
           <Logo />
 
           {/* Deskop */}
-          <div className="hidden lg:block">
-            <nav>
-              <ul className="flex gap-[40px]">
-                <li>
-                  <a
-                    className="text text-[24px] text-blue-sky font-medium leading-1.2"
-                    href="#goal"
-                  >
-                    Мета
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="text text-[24px] text-blue-sky font-medium leading-1.2"
-                    href="#about"
-                  >
-                    Про Госпіталь
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="text text-[24px] text-blue-sky font-medium leading-1.2"
-                    href="#needs"
-                  >
-                    Потреби
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
+          <nav className="hidden sLg:flex items-center justify-center">
+            <ul className="flex gap-[40px] mr-[72px] lg:mr-[128px]">
+              <li>
+                <a
+                  className="text text-[24px] text-blue-sky hover:text-black transition-all font-medium leading-1.2"
+                  href="#goal"
+                >
+                  Мета
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text text-[24px] text-blue-sky hover:text-black transition-all font-medium leading-1.2"
+                  href="#about"
+                >
+                  Про Госпіталь
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text text-[24px] text-blue-sky hover:text-black transition-all font-medium leading-1.2"
+                  href="#needs"
+                >
+                  Потреби
+                </a>
+              </li>
+            </ul>
+            <div className="hidden sLg:block mr-[0px]">
+              <ShareButton />
+            </div>
+          </nav>
           {/* ------ */}
 
-          <div className=" hidden md:block md:mr-[166px] lg:mr-[0px]">
+          <div className="hidden md:block md:mr-[166px] sLg:hidden">
             <ShareButton />
           </div>
 
           <button
             type="button"
             onClick={() => {
-              setIsMenu(!isMenu);
+              setMenu(!isMenu);
             }}
-            className="inline-block w-[40px] h-[40px] lg:hidden"
+            className="inline-block w-[40px] h-[40px] sLg:hidden"
           >
             <CgMenuRound className="w-full h-full text-blue-sky" />
           </button>
         </div>
         {/* Mobile Menu */}
         <div
-          className={`md:hidden lg:hidden absolute top-0 left-0 md:top-[90px] w-full h-[252px] md:h-full px-[12px] py-[28px] md:p-[32px] bg-blue-sky rounded-2xl transition-all duration-200
+          className={`md:hidden lg:hidden absolute top-0 left-0 md:top-[90px] w-full h-[250px] md:h-full py-[24px] md:p-[32px] bg-blue-sky rounded-2xl transition-all duration-200
             `}
           style={{
             top: isMenu ? 0 : -252,
@@ -73,34 +74,37 @@ const Header = () => {
           <button
             type="button"
             onClick={() => {
-              setIsMenu(!isMenu);
+              setMenu(!isMenu);
             }}
             className="absolute top-[10px] right-[10px] w-[35px] h-[35px]"
           >
             <AiOutlineCloseCircle className="w-[35px] h-[35px] text-black" />
           </button>
           <nav className="w-[200px] mx-auto text-center">
-            <ul className="flex flex-col gap-[24px] md:gap-[40px] mb-[24px] md:mb-[0px] ">
+            <ul className="flex flex-col gap-[24px] md:gap-[40px] mb-[30px] md:mb-[0px] ">
               <li>
                 <a
-                  className="text text-[20px] md:text[24px] font-medium leading-1.2"
+                  className="text text-[20px] md:text[24px] text-dark-blue font-medium leading-1.2"
                   href="#goal"
+                  onClick={() => setMenu(false)}
                 >
                   Мета
                 </a>
               </li>
               <li>
                 <a
-                  className="text text-[20px] md:text[24px] font-medium leading-1.2"
+                  className="text text-[20px] md:text[24px] text-dark-blue font-medium leading-1.2"
                   href="#about"
+                  onClick={() => setMenu(false)}
                 >
                   Про Госпіталь
                 </a>
               </li>
               <li>
                 <a
-                  className="text text-[20px] md:text[24px] font-medium leading-1.2"
+                  className="text text-[20px] md:text[24px] text-dark-blue font-medium leading-1.2"
                   href="#needs"
+                  onClick={() => setMenu(false)}
                 >
                   Потреби
                 </a>
@@ -113,7 +117,7 @@ const Header = () => {
         </div>
         {/* Tablet Menu */}
         <div
-          className="hidden lg:hidden absolute z-50 top-0 md:flex justify-center items-center bg-blue-sky h-full rounded-2xl transition-all duration-200"
+          className="hidden lg:hidden absolute z-50 top-0 md:flex justify-center items-center bg-blue-sky h-[93px] rounded-2xl transition-all duration-300"
           style={{
             width: isMenu ? '100%' : '10%',
             right: isMenu ? 0 : 100,
@@ -125,8 +129,8 @@ const Header = () => {
               <ul className="flex gap-[40px] mr-[250px]">
                 <li>
                   <a
-                    onClick={() => setIsMenu(false)}
-                    className="text text-[20px] md:text[24px] font-medium leading-1.2"
+                    onClick={() => setMenu(false)}
+                    className="text text-[20px] md:text[24px] text-dark-blue font-medium leading-1.2"
                     href="#goal"
                   >
                     Мета
@@ -134,8 +138,8 @@ const Header = () => {
                 </li>
                 <li>
                   <a
-                    onClick={() => setIsMenu(false)}
-                    className="text text-[20px] md:text[24px] font-medium leading-1.2"
+                    onClick={() => setMenu(false)}
+                    className="text text-[20px] md:text[24px] text-dark-blue font-medium leading-1.2"
                     href="#about"
                   >
                     Про Госпіталь
@@ -143,8 +147,8 @@ const Header = () => {
                 </li>
                 <li>
                   <a
-                    onClick={() => setIsMenu(false)}
-                    className="text text-[20px] md:text[24px] font-medium leading-1.2"
+                    onClick={() => setMenu(false)}
+                    className="text text-[20px] md:text[24px] text-dark-blue font-medium leading-1.2"
                     href="#needs"
                   >
                     Потреби
@@ -154,7 +158,7 @@ const Header = () => {
               <button
                 type="button"
                 onClick={() => {
-                  setIsMenu(!isMenu);
+                  setMenu(!isMenu);
                 }}
                 className="w-[35px] h-[35px]"
               >
